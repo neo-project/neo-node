319230c8b02b846792babed94b178eade44816e4
Wojtek <wojciech.turyn@coinfoundation.io>
2018-07-12 18:40:16 +0200

    cleanup

--- neo-cli/Settings.cs
+++ neo-cli/Settings.cs
@@ -74,14 +74,13 @@ namespace Neo
 
         public UnlockWalletSettings(IConfigurationSection section)
         {
-            if (section.Exists()) 
+            if (section.Exists())
             {
                 this.Path = section.GetSection("Path").Value;
                 this.Password = section.GetSection("Password").Value;
                 this.StartConsensus = bool.Parse(section.GetSection("StartConsensus").Value);
                 this.IsActive = bool.Parse(section.GetSection("IsActive").Value);     
             }
-            
         }
     }
 }
