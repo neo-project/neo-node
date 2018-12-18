aefdc1bbcc4dd9741310717ec1eaa774ad56c247
jsolman <jeff@aphelion.org>
2018-12-17 17:32:12 -0800

    Don't run out of gas when testing for GAS required for deployment.

--- neo-cli/Shell/MainService.cs
+++ neo-cli/Shell/MainService.cs
@@ -175,7 +175,7 @@ namespace Neo.Shell
                     if (tx.Inputs == null) tx.Inputs = new CoinReference[0];
                     if (tx.Outputs == null) tx.Outputs = new TransactionOutput[0];
                     if (tx.Witnesses == null) tx.Witnesses = new Witness[0];
-                    ApplicationEngine engine = ApplicationEngine.Run(tx.Script, tx);
+                    ApplicationEngine engine = ApplicationEngine.Run(tx.Script, tx, null, true);
                     StringBuilder sb = new StringBuilder();
                     sb.AppendLine($"VM State: {engine.State}");
                     sb.AppendLine($"Gas Consumed: {engine.GasConsumed}");
