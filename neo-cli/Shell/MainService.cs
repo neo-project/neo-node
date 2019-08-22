8c7fb4a17665fea9d472b969d2caa2985b60cdab
yongjiema <20391402+yongjiema@users.noreply.github.com>
2019-08-22 01:47:35 +0000

    Adjust the output format of show state command

--- neo-cli/Shell/MainService.cs
+++ neo-cli/Shell/MainService.cs
@@ -991,7 +991,7 @@ namespace Neo.Shell
                     foreach (RemoteNode node in LocalNode.Singleton.GetRemoteNodes().Take(Console.WindowHeight - 2).ToArray())
                     {
                         WriteLineWithoutFlicker(
-                            $"  ip: {node.Remote.Address}\tport: {node.Remote.Port}\tlisten: {node.ListenerTcpPort}\theight: {node.LastBlockIndex}");
+                            $"  ip: {node.Remote.Address.ToString().PadRight(15)}\tport: {node.Remote.Port}\tlisten: {node.ListenerTcpPort}\theight: {node.LastBlockIndex}");
                         linesWritten++;
                     }
 
