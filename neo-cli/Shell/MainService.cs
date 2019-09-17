5ab3ed9c5fe9dfc7d9a7925683a7679e6a6db08c
Shargon <shargon@gmail.com>
2019-09-17 15:08:43 +0200

    Clean code

--- neo-cli/Shell/MainService.cs
+++ neo-cli/Shell/MainService.cs
@@ -227,6 +227,7 @@ namespace Neo.Shell
             {
                 throw new ArgumentException(nameof(manifestFilePath));
             }
+
             var manifest = ContractManifest.Parse(File.ReadAllText(manifestFilePath));
 
             // Read nef
