44690264afa7207f3df3e3c26d60bb04cddb2d1b
Shargon <shargon@gmail.com>
2020-02-24 12:31:55 +0100

    Remove enum values

--- neo-cli/CommandParser/CommandTokenType.cs
+++ neo-cli/CommandParser/CommandTokenType.cs
@@ -2,8 +2,8 @@ namespace Neo.CommandParser
 {
     public enum CommandTokenType : byte
     {
-        String = 0x01,
-        Space = 0x02,
-        Quote = 0x03,
+        String,
+        Space,
+        Quote,
     }
 }
