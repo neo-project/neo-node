3dc713912346b69ec2e552733acf1a742aca01b1
Owen Zhang <38493437+superboyiii@users.noreply.github.com>
2020-03-02 17:00:35 +0800

    Add cosigner support witness
    Add cosigner support witness

--- neo-cli/CLI/MainService.Contracts.cs
+++ neo-cli/CLI/MainService.Contracts.cs
@@ -46,11 +46,47 @@ namespace Neo.CLI
         /// <param name="scriptHash">Script hash</param>
         /// <param name="operation">Operation</param>
         /// <param name="contractParameters">Contract parameters</param>
-        [Category("Contract Commands")]
-        [ConsoleCommand("invoke")]
-        private void OnInvokeCommand(UInt160 scriptHash, string operation, [CaptureWholeArgument] JArray contractParameters = null)
+        /// <param name="witnessAddress">Witness address</param>
+        [ConsoleCommand("invoke", HelpCategory = "Contract Commands")]
+        private void OnInvokeCommand(UInt160 scriptHash, string operation, JArray contractParameters = null,  string[] witnessAddress = null)
         {
             List<ContractParameter> parameters = new List<ContractParameter>();
+            List<Cosigner> signCollection = new List<Cosigner>();
+
+            if (!NoWallet() && witnessAddress != null)
+            {
+                try
+                {
+                    foreach (var witness in witnessAddress)
+                    {
+                        witness.ToScriptHash();
+                    }
+                }
+                catch
+                {
+                    witnessAddress = null;
+                }
+
+                using (SnapshotView snapshot = Blockchain.Singleton.GetSnapshot())
+                {
+                    UInt160[] accounts = CurrentWallet.GetAccounts().Where(p => !p.Lock && !p.WatchOnly).Select(p => p.ScriptHash).Where(p => NativeContract.GAS.BalanceOf(snapshot, p).Sign > 0).ToArray();
+                    foreach (var signAccount in accounts)
+                    {
+                        if (witnessAddress is null)
+                        {
+                            break;
+                        }
+                        foreach (var witness in witnessAddress)
+                        {
+                            if (witness.Equals(signAccount.ToAddress()))
+                            {
+                                signCollection.Add(new Cosigner() { Account = signAccount });
+                            }
+                        } 
+                    }
+                }
+            }
+            
             if (contractParameters != null)
             {
                 foreach (var contractParameter in contractParameters)
@@ -64,7 +100,7 @@ namespace Neo.CLI
                 Sender = UInt160.Zero,
                 Attributes = Array.Empty<TransactionAttribute>(),
                 Witnesses = Array.Empty<Witness>(),
-                Cosigners = Array.Empty<Cosigner>()
+                Cosigners = signCollection.ToArray()
             };
 
             using (ScriptBuilder scriptBuilder = new ScriptBuilder())
