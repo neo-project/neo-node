6edbeaff8f86b540d32abf66c80fc42b9d1c2e2a
ZhangHaoqiang <gripzhang@outlook.com>
2020-04-15 11:46:16 +0800

    Update MainService.cs

--- neo-cli/CLI/MainService.cs
+++ neo-cli/CLI/MainService.cs
@@ -140,8 +140,9 @@ namespace Neo.CLI
                     {
                         UserWallet wallet = UserWallet.Create(path, password);
                         WalletAccount account = wallet.CreateAccount();
-                        Console.WriteLine($"address: {account.Address}");
-                        Console.WriteLine($" pubkey: {account.GetKey().PublicKey.EncodePoint(true).ToHexString()}");
+                        Console.WriteLine($"    address: {account.Address}");
+                        Console.WriteLine($"     pubkey: {account.GetKey().PublicKey.EncodePoint(true).ToHexString()}");
+                        Console.WriteLine($"script hash: {account.ScriptHash}");
                         CurrentWallet = wallet;
                     }
                     break;
@@ -151,8 +152,9 @@ namespace Neo.CLI
                         wallet.Unlock(password);
                         WalletAccount account = wallet.CreateAccount();
                         wallet.Save();
-                        Console.WriteLine($"address: {account.Address}");
-                        Console.WriteLine($" pubkey: {account.GetKey().PublicKey.EncodePoint(true).ToHexString()}");
+                        Console.WriteLine($"    address: {account.Address}");
+                        Console.WriteLine($"     pubkey: {account.GetKey().PublicKey.EncodePoint(true).ToHexString()}");
+                        Console.WriteLine($"script hash: {account.ScriptHash}");
                         CurrentWallet = wallet;
                     }
                     break;
