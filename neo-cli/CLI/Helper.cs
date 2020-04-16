dfaccace6dea7bf5cbad631b530e07b44c02f23e
Luchuan <luchuan@neo.org>
2020-04-16 13:34:49 +0800

    fix tojson

--- neo-cli/CLI/Helper.cs
+++ neo-cli/CLI/Helper.cs
@@ -1,5 +1,4 @@
 using Neo.IO.Json;
-using Neo.SmartContract;
 using Neo.VM.Types;
 using System;
 using System.Collections.Generic;
@@ -34,73 +33,45 @@ namespace Neo.CLI
             return ToJson(item, null);
         }
 
-        private static JObject ToJson(StackItem item, List<(StackItem, JObject)> context = null)
+        private static JObject ToJson(StackItem item, HashSet<StackItem> context)
         {
-            if (item is null) throw new ArgumentNullException();
-            JObject parameter = null;
+            JObject json = new JObject();
+            json["type"] = item.Type;
             switch (item)
             {
                 case Array array:
-                    if (context is null)
-                        context = new List<(StackItem, JObject)>();
-                    else
-                        (_, parameter) = context.FirstOrDefault(p => ReferenceEquals(p.Item1, item));
-                    if (parameter is null)
-                    {
-                        parameter = new JObject();
-                        parameter["type"] = ContractParameterType.Array;
-                        context.Add((item, parameter));
-                        parameter["value"] = array.Select(p => ToJson(p, context)).ToArray();
-                    }
+                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
+                    if (!context.Add(array)) throw new InvalidOperationException();
+                    json["value"] = new JArray(array.Select(p => ToJson(p, context)));
                     break;
-                case Map map:
-                    if (context is null)
-                        context = new List<(StackItem, JObject)>();
-                    else
-                        (_, parameter) = context.FirstOrDefault(p => ReferenceEquals(p.Item1, item));
-                    if (parameter is null)
-                    {
-                        parameter = new JObject();
-                        parameter["type"] = ContractParameterType.Map;
-                        context.Add((item, parameter));
-                        parameter["value"] = map.Select(p =>
-                        {
-                            JObject item = new JObject();
-                            item["key"] = ToJson(p.Key, context);
-                            item["value"] = ToJson(p.Value, context);
-                            return item;
-                        }).ToArray();
-                    }
-                    break;
-                case Boolean _:
-                    parameter = new JObject();
-                    parameter["type"] = ContractParameterType.Boolean;
-                    parameter["value"] = item.ToBoolean();
-                    break;
-                case ByteString byteString:
-                    parameter = new JObject();
-                    parameter["type"] = ContractParameterType.ByteArray;
-                    parameter["value"] = Convert.ToBase64String(((ByteString)item).Span.ToArray());
+                case Boolean boolean:
+                    json["value"] = boolean.ToBoolean();
                     break;
                 case Buffer buffer:
-                    parameter = new JObject();
-                    parameter["type"] = ContractParameterType.ByteArray;
-                    parameter["value"] = Convert.ToBase64String(((Buffer)item).InnerBuffer.ToArray());
+                    json["value"] = Convert.ToBase64String(buffer.InnerBuffer);
                     break;
-                case Integer i:
-                    parameter = new JObject();
-                    parameter["type"] = ContractParameterType.Integer;
-                    parameter["value"] = i.ToBigInteger().ToString();
+                case ByteString byteString:
+                    json["value"] = Convert.ToBase64String(byteString.Span);
                     break;
-                case Null _:
-                    parameter = new JObject();
-                    parameter["type"] = ContractParameterType.Any;
-                    parameter["value"] = null;
+                case Integer integer:
+                    json["value"] = integer.ToBigInteger().ToString();
                     break;
-                default:
-                    throw new ArgumentException("Not supported to Json.");
+                case Map map:
+                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
+                    if (!context.Add(map)) throw new InvalidOperationException();
+                    json["value"] = new JArray(map.Select(p =>
+                    {
+                        JObject item = new JObject();
+                        item["key"] = ToJson(p.Key, context);
+                        item["value"] = ToJson(p.Value, context);
+                        return item;
+                    }));
+                    break;
+                case Pointer pointer:
+                    json["value"] = pointer.Position;
+                    break;
             }
-            return parameter;
+            return json;
         }
     }
 }
