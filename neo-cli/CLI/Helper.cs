f317dda4dfb5538227fe563b4551b3da508aa793
Luchuan <luchuan@neo.org>
2020-04-16 13:39:23 +0800

    use Neo.3.0.0-CI00892

--- neo-cli/CLI/Helper.cs
+++ neo-cli/CLI/Helper.cs
@@ -28,50 +28,5 @@ namespace Neo.CLI
 
             return input == "yes" || input == "y";
         }
-        public static JObject ToJson(this StackItem item)
-        {
-            return ToJson(item, null);
-        }
-
-        private static JObject ToJson(StackItem item, HashSet<StackItem> context)
-        {
-            JObject json = new JObject();
-            json["type"] = item.Type;
-            switch (item)
-            {
-                case Array array:
-                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
-                    if (!context.Add(array)) throw new InvalidOperationException();
-                    json["value"] = new JArray(array.Select(p => ToJson(p, context)));
-                    break;
-                case Boolean boolean:
-                    json["value"] = boolean.ToBoolean();
-                    break;
-                case Buffer buffer:
-                    json["value"] = Convert.ToBase64String(buffer.InnerBuffer);
-                    break;
-                case ByteString byteString:
-                    json["value"] = Convert.ToBase64String(byteString.Span);
-                    break;
-                case Integer integer:
-                    json["value"] = integer.ToBigInteger().ToString();
-                    break;
-                case Map map:
-                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
-                    if (!context.Add(map)) throw new InvalidOperationException();
-                    json["value"] = new JArray(map.Select(p =>
-                    {
-                        JObject item = new JObject();
-                        item["key"] = ToJson(p.Key, context);
-                        item["value"] = ToJson(p.Value, context);
-                        return item;
-                    }));
-                    break;
-                case Pointer pointer:
-                    json["value"] = pointer.Position;
-                    break;
-            }
-            return json;
-        }
     }
 }
