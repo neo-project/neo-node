75dc442467c02e3071ec4c7a32705df259361e4c
ProDog <gripzhang@outlook.com>
2020-04-16 10:57:55 +0800

    remove useless references

--- neo-cli/CLI/MainService.Vote.cs
+++ neo-cli/CLI/MainService.Vote.cs
@@ -11,9 +11,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Neo.IO;
 using Neo.Cryptography.ECC;
-using System.IO;
-using Neo.SmartContract.Manifest;
-using System.Text;
 
 namespace Neo.CLI
 {
