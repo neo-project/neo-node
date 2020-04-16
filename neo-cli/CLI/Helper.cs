d53c70d2856c0e08df82f1467615c37fcac2dc19
Luchuan <luchuan@neo.org>
2020-04-16 15:02:56 +0800

    fix StackItem.ToJson

--- neo-cli/CLI/Helper.cs
+++ neo-cli/CLI/Helper.cs
@@ -1,3 +1,13 @@
+using Neo.IO.Json;
+using Neo.SmartContract;
+using Neo.VM.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Array = Neo.VM.Types.Array;
+using Boolean = Neo.VM.Types.Boolean;
+using Buffer = Neo.VM.Types.Buffer;
+
 namespace Neo.CLI
 {
     internal static class Helper
@@ -18,6 +28,52 @@ namespace Neo.CLI
             input = input.ToLowerInvariant();
 
             return input == "yes" || input == "y";
+        }
+
+        public static JObject ToJson(this StackItem item)
+        {
+            return ToJson(item, null);
+        }
+
+        private static JObject ToJson(StackItem item, HashSet<StackItem> context)
+        {
+            JObject json = new JObject();
+            json["type"] = item.Type;
+            switch (item)
+            {
+                case Array array:
+                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
+                    if (!context.Add(array)) throw new InvalidOperationException();
+                    json["value"] = new JArray(array.Select(p => ToJson(p, context)));
+                    break;
+                case Boolean boolean:
+                    json["value"] = boolean.ToBoolean();
+                    break;
+                case Buffer buffer:
+                    json["value"] = Convert.ToBase64String(buffer.InnerBuffer);
+                    break;
+                case ByteString byteString:
+                    json["value"] = Convert.ToBase64String(byteString.Span);
+                    break;
+                case Integer integer:
+                    json["value"] = integer.ToBigInteger().ToString();
+                    break;
+                case Map map:
+                    context ??= new HashSet<StackItem>(ReferenceEqualityComparer.Default);
+                    if (!context.Add(map)) throw new InvalidOperationException();
+                    json["value"] = new JArray(map.Select(p =>
+                    {
+                        JObject item = new JObject();
+                        item["key"] = ToJson(p.Key, context);
+                        item["value"] = ToJson(p.Value, context);
+                        return item;
+                    }));
+                    break;
+                case Pointer pointer:
+                    json["value"] = pointer.Position;
+                    break;
+            }
+            return json;
         }
     }
 }
