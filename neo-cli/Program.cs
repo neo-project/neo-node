4e5130b305d0806a04ba0929f78e1175952a638e
Shargon <shargon@gmail.com>
2020-05-03 10:09:12 +0200

    Remove error.logs

--- neo-cli/Program.cs
+++ neo-cli/Program.cs
@@ -6,7 +6,6 @@ namespace Neo
     {
         static void Main(string[] args)
         {
-            _ = new Logger();
             var mainService = new MainService();
             mainService.Run(args);
         }
