82033b46d36a69b31e24e6f421e5e174a2ca0d88
Shargon <shargon@gmail.com>
2020-05-04 15:23:50 +0200

    Remove log logic (#577)
    * Log akka errors
    
    * dotnet-format
    
    * Clean using
    
    * add readonly
    
    * Remove error.logs
    
    * Update nuget

--- neo-cli/Program.cs
+++ neo-cli/Program.cs
@@ -1,51 +1,13 @@
 using Neo.CLI;
-using System;
-using System.IO;
 
 namespace Neo
 {
     static class Program
     {
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            using (FileStream fs = new FileStream("error.log", FileMode.Create, FileAccess.Write, FileShare.None))
-            using (StreamWriter w = new StreamWriter(fs))
-                if (e.ExceptionObject is Exception ex)
-                {
-                    PrintErrorLogs(w, ex);
-                }
-                else
-                {
-                    w.WriteLine(e.ExceptionObject.GetType());
-                    w.WriteLine(e.ExceptionObject);
-                }
-        }
-
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             var mainService = new MainService();
             mainService.Run(args);
-        }
-
-        private static void PrintErrorLogs(StreamWriter writer, Exception ex)
-        {
-            writer.WriteLine(ex.GetType());
-            writer.WriteLine(ex.Message);
-            writer.WriteLine(ex.StackTrace);
-            if (ex is AggregateException ex2)
-            {
-                foreach (Exception inner in ex2.InnerExceptions)
-                {
-                    writer.WriteLine();
-                    PrintErrorLogs(writer, inner);
-                }
-            }
-            else if (ex.InnerException != null)
-            {
-                writer.WriteLine();
-                PrintErrorLogs(writer, ex.InnerException);
-            }
         }
     }
 }
