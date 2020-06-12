6b9c645badf82f23954c910a1bfa9022b1275178
陈志同 <chenzhitong12@hotmail.com>
2020-06-12 16:11:51 +0800

    update

--- neo-cli/CLI/MainService.Native.cs
+++ neo-cli/CLI/MainService.Native.cs
@@ -1,21 +1,7 @@
-using Akka.Actor;
 using Neo.ConsoleService;
-using Neo.Cryptography.ECC;
-using Neo.IO.Json;
-using Neo.Ledger;
-using Neo.Network.P2P;
-using Neo.Network.P2P.Payloads;
-using Neo.Persistence;
-using Neo.SmartContract;
 using Neo.SmartContract.Native;
-using Neo.Wallets;
-using Neo.Wallets.NEP6;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Numerics;
-using System.Threading.Tasks;
 
 namespace Neo.CLI
 {
