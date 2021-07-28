7e7d8ff745eadb9d9cb8958a5351ae6d51c99912
HaoqiangZhang <gripzhang@outlook.com>
2021-07-28 22:50:15 +0800

    Modify the maximum input length (#801)

--- Neo.ConsoleService/ConsoleServiceBase.cs
+++ Neo.ConsoleService/ConsoleServiceBase.cs
@@ -590,6 +590,7 @@ namespace Neo.ConsoleService
                 catch { }
 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(), Encoding.Default, false, ushort.MaxValue));
 
             while (_running)
             {
