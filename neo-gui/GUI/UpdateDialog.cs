dd0dba9675d8ea505d6ca7e21173e1d18feff674
Jinghui Liao <jinghui@wayne.edu>
2021-08-15 18:50:51 +0800

    fix neo-gui copyright

--- neo-gui/GUI/UpdateDialog.cs
+++ neo-gui/GUI/UpdateDialog.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2014-2021 NEO GLOBAL DEVELOPMENT.
 // 
-// The neo-consolservice is free software distributed under the MIT software license, 
+// The neo-gui is free software distributed under the MIT software license, 
 // see the accompanying file LICENSE in the main directory of the
 // project or http://www.opensource.org/licenses/mit-license.php 
 // for more details.
