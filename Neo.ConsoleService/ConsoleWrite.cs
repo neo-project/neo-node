935db80d1e447da97f44152b55d373f91bf77559
Erik Zhang <erik@neo.org>
2021-08-18 16:36:04 +0800

    static class

--- Neo.ConsoleService/ConsoleWrite.cs
+++ Neo.ConsoleService/ConsoleWrite.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Neo.ConsoleService
 {
-    public class ConsoleWrite
+    public static class ConsoleWrite
     {
         public static void Info(params string[] values)
         {
