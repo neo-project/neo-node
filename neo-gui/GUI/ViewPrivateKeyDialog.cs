a2b705d919a3cbe57b7b73045dfafa510f9e2f2e
Jinghui Liao <jinghui@wayne.edu>
2021-08-18 18:29:51 +0800

    update copyright

--- neo-gui/GUI/ViewPrivateKeyDialog.cs
+++ neo-gui/GUI/ViewPrivateKeyDialog.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2016-2021 The Neo Project.
 // 
-// The Neo.GUI is free software distributed under the MIT software 
+// The neo-gui is free software distributed under the MIT software 
 // license, see the accompanying file LICENSE in the main directory of
 // the project or http://www.opensource.org/licenses/mit-license.php 
 // for more details.
