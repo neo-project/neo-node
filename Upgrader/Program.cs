3bd8e70fac97477bb2328426df9c4b3bf8c4ed28
Jinghui Liao <jinghui@wayne.edu>
2021-10-17 22:10:13 -0400

    Update Program.cs

--- Upgrader/Program.cs
+++ Upgrader/Program.cs
@@ -96,7 +96,7 @@ namespace upgrader
                 {
                     zip.ExtractToDirectory(Temp, true);
                     CopyFilesRecursively($"{Temp}/neo-cli", ".");
-                    Console.WriteLine($"{file}\t upgrade successfully");
+                    Console.WriteLine($"{file}\t update completed.");
                 }
                 catch (IOException)
                 {
@@ -131,7 +131,7 @@ namespace upgrader
                     var temp = Path.Combine(Path.GetTempPath());
                     zip.ExtractToDirectory($"{temp}/{pluginName}", true);
                     CopyFilesRecursively($"{temp}/{pluginName}/", $"./");
-                    Console.WriteLine($"{pluginName}\t upgrade successfully");
+                    Console.WriteLine($"{pluginName}\t update completed.");
                 }
                 catch (IOException ex)
                 {
