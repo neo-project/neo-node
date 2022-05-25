9dafb2f411c20686a59e240d5c0f48b3088a590f
Jinghui Liao <jinghui@wayne.edu>
2022-05-24 20:19:23 -0400

    update plugin existance check

--- neo-cli/CLI/MainService.Plugins.cs
+++ neo-cli/CLI/MainService.Plugins.cs
@@ -1,10 +1,10 @@
-// Copyright (C) 2016-2021 The Neo Project.
-// 
-// The neo-cli is free software distributed under the MIT software 
+// Copyright (C) 2016-2022 The Neo Project.
+//
+// The neo-cli is free software distributed under the MIT software
 // license, see the accompanying file LICENSE in the main directory of
-// the project or http://www.opensource.org/licenses/mit-license.php 
+// the project or http://www.opensource.org/licenses/mit-license.php
 // for more details.
-// 
+//
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
@@ -174,7 +174,7 @@ namespace Neo.CLI
         /// <returns></returns>
         private static bool PluginExists(string pluginName)
         {
-            return File.Exists($"Plugins/{pluginName}/{pluginName}.dll");
+            return Plugin.Plugins.Any(p=>p.Name == pluginName);
         }
 
         /// <summary>
