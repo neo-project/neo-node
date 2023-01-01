36a830ccf54d2456c78b8fc6ca2bc6aa57db4519
Jinghui Liao <jinghui@wayne.edu>
2022-12-31 22:00:34 -0500

    update copyright

--- neo-cli/CLI/MainService.Plugins.cs
+++ neo-cli/CLI/MainService.Plugins.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2016-2022 The Neo Project.
+// Copyright (C) 2016-2023 The Neo Project.
 // The neo-cli is free software distributed under the MIT software 
 // license, see the accompanying file LICENSE in the main directory of
 // the project or http://www.opensource.org/licenses/mit-license.php
