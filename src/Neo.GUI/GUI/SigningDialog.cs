db41ee76c6fb35a543d96500625e58ddff256e39
Shargon <shargon@gmail.com>
2026-01-03 10:03:58 +0100

    [N4] Happy new year 2026 (#957)
    Happy new Year

--- src/Neo.GUI/GUI/SigningDialog.cs
+++ src/Neo.GUI/GUI/SigningDialog.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2015-2025 The Neo Project.
+// Copyright (C) 2015-2026 The Neo Project.
 //
 // SigningDialog.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
@@ -10,7 +10,6 @@
 // modifications are permitted.
 
 using Neo.Cryptography;
-using Neo.Extensions;
 using Neo.Properties;
 using Neo.Wallets;
 using System.Text;
