d8a88ec1a23052d7a49d852706a2d793fa1089ef
Alvaro <amjarag@gmail.com>
2026-01-30 00:43:22 +0100

    2026 into directory props

--- tests/Neo.Cryptography.MPTTrie.Tests/Cryptography/MPTTrie/UT_Trie.cs
+++ tests/Neo.Cryptography.MPTTrie.Tests/Cryptography/MPTTrie/UT_Trie.cs
@@ -55,7 +55,10 @@ class TestSnapshot : IStoreSnapshot
     {
         return _store.TryGetValue(StoreKey(key), out value);
     }
-
+    public IEnumerable<(byte[] Key, byte[] Value)> FindRange(byte[] start, byte[] end, SeekDirection direction = SeekDirection.Forward)
+    {
+        throw new NotImplementedException();
+    }
     public void Dispose() { throw new NotImplementedException(); }
 
     public int Size => _store.Count;
