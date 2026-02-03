bb817071278b1e9076e1bf8fab254a2e40ec442a
superboyiii <573504781@qq.com>
2026-02-03 14:56:28 +0800

    fix

--- src/Neo.CLI/Docker/start.sh
+++ src/Neo.CLI/Docker/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# 启动 dotnet CLI 后台运行，日志输出到 neo.log
+# Start CLI in background, log output to neo.log
 screen -dmS neo bash -c "./neo-cli/neo-cli > neo.log 2>&1"
 
-# 等待 neo.log 出现
 while [ ! -f neo.log ]; do
   sleep 0.5
 done
 
-# 实时查看日志（保持容器不退出）
+# Timely check log
 tail -f neo.log
