8fdcb43fe45047470be676201ccb0a888a0ca831
superboyiii <573504781@qq.com>
2026-06-24 12:09:00 +0800

    produce single file for release

--- src/Neo.CLI/Docker/prepare-node.sh
+++ src/Neo.CLI/Docker/prepare-node.sh
@@ -1,6 +1,7 @@
 echo "Downloading neo node $1"
-wget  https://github.com/neo-project/neo-node/releases/download/$1/neo-cli-linux-x64.zip
-unzip neo-cli-linux-x64.zip
+ARCHIVE="neo-cli.$1-linux-x64.tar.gz"
+wget "https://github.com/neo-project/neo-node/releases/download/$1/$ARCHIVE"
+tar -xzf "$ARCHIVE"
 
 if [ -d neo-cli ]; then
     PLUGIN_DIR=./neo-cli
