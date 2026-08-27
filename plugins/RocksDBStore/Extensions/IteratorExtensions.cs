fb4b9b92ea54ce0e3b4ab8c7bbc908b6402c3b73
Shargon <shargon@gmail.com>
2026-08-27 17:59:11 +0200

    fix comment

--- plugins/RocksDBStore/Extensions/IteratorExtensions.cs
+++ plugins/RocksDBStore/Extensions/IteratorExtensions.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2026 The Neo Project.
 //
-// Iterator.cs file belongs to the neo project and is free
+// IteratorExtensions.cs file belongs to the neo project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php
